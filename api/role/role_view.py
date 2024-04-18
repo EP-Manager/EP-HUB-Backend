7645cf195f83955dbf2e711bc321ded85d037e35
Jerom <jeromjomanthara@gmail.com>
2024-04-18 13:23:34 +0530

    feat(api): add role and user role apis

---
+++ api/role/role_view.py
@@ -0,0 +1,95 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from api.models import Role, UserRoleLink
+from api.utils import CustomResponse, get_user_id, RoleList, allowed_roles
+from .role_serializer import RoleDropDownSerializer, RoleListSerializer, RoleCreateEditSerializer, UserRoleListSerializer, UserRoleCreateSerializer, UserRoleUpdateSerializer
+
+
+class RoleDropDownAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @allowed_roles([RoleList.ADMIN.value])
+    def get(self, request):
+        roles = Role.objects.all()
+        serializer = RoleDropDownSerializer(roles, many=True)
+        return CustomResponse(message="successfully obtained roles", data=serializer.data).success_response()
+
+class RoleAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @allowed_roles([RoleList.ADMIN.value])
+    def get(self, request):
+        roles = Role.objects.all()
+        serializer = RoleListSerializer(roles, many=True)
+        return CustomResponse(message="successfully obtained roles", data=serializer.data).success_response()
+    
+    @allowed_roles([RoleList.ADMIN.value])
+    def post(self, request):
+        user_id = get_user_id(request)
+        print(user_id)
+        serializer = RoleCreateEditSerializer(data=request.data, context={'request': request, 'user_id': user_id})
+
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(message="successfully created role", data=serializer.data).success_response()
+        return CustomResponse(message="failed to create role", data=serializer.errors).failure_reponse()
+    
+    @allowed_roles([RoleList.ADMIN.value])
+    def patch(self, request, role_id):
+        user_id = get_user_id(request)
+        role = Role.objects.filter(id=role_id).first()
+        if not role:
+            return CustomResponse(message="role does not exist").failure_reponse()
+        serializer = RoleCreateEditSerializer(role, data=request.data, context={'request': request, 'user_id': user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(message="successfully updated role", data=serializer.data).success_response()
+        return CustomResponse(message="failed to update role", data=serializer.errors).failure_reponse()
+    
+    @allowed_roles([RoleList.ADMIN.value])
+    def delete(self, request, role_id):
+        role = Role.objects.filter(id=role_id).first()
+        if not role:
+            return CustomResponse(message="role does not exist").failure_reponse()
+        role.delete()
+        return CustomResponse(message="successfully deleted role").success_response()
+    
+class UserRoleView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @allowed_roles([RoleList.ADMIN.value])
+    def get(self, request):
+        user_roles = UserRoleLink.objects.all()
+        serializer = UserRoleListSerializer(user_roles, many=True)
+        return CustomResponse(message="successfully obtained roles", data=serializer.data).success_response()
+    
+    @allowed_roles([RoleList.ADMIN.value])
+    def post(self, request):
+        user_id = get_user_id(request)
+        serializer = UserRoleCreateSerializer(data=request.data, context={'request': request, 'user_id': user_id})
+
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(message="successfully created role", data=serializer.data).success_response()
+        return CustomResponse(message="failed to create role", data=serializer.errors).failure_reponse()
+    
+    @allowed_roles([RoleList.ADMIN.value])
+    def patch(self, request, user_role_id):
+        user_id = get_user_id(request)
+        user_role = UserRoleLink.objects.filter(id=user_role_id).first()
+        if not user_role:
+            return CustomResponse(message="user role does not exist").failure_reponse()
+        serializer = UserRoleUpdateSerializer(user_role, data=request.data, context={'request': request, 'user_id': user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(message="successfully updated role", data=serializer.data).success_response()
+        return CustomResponse(message="failed to update role", data=serializer.errors).failure_reponse()
+    
+    @allowed_roles([RoleList.ADMIN.value])
+    def delete(self, request, user_role_id):
+        user_role = UserRoleLink.objects.filter(id=user_role_id).first()
+        if not user_role:
+            return CustomResponse(message="user role does not exist").failure_reponse()
+        user_role.delete()
+        return CustomResponse(message="successfully deleted role").success_response()
--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('district/', include('api.district.district_urls')),
+
+    path('role/', include('api.role.role_urls')),
     
 ]
